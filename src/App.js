cbf84cdc08446699a2137810ccdf92c04d83e8ce
Juan Rodriguez <juan109rodriguez@gmail.com>
2020-11-20 17:03:28 -0600

    hide api key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BusinessList from "./components/BusinessList/BusinessList.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
+import Yelp from "./util/Yelp";
+const dotenv = require("dotenv").config();
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { businesses: [] };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    console.log("searching yelp with" + term + "," + location + "," + sortBy);
+
+    Yelp.search(term, location, sortBy).then((businesses) => {
+      try {
+        this.setState({ businesses: businesses });
+        console.log(this.state.businesses);
+      } catch (error) {
+        if (
+          this.state.businesses.length === 0 ||
+          this.state.businesses === undefined
+        ) {
+          this.setState({ businesses: "empty" });
+        }
+      }
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Good Eats</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
